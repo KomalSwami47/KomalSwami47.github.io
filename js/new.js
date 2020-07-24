bdb69a121c320687cd089ff8610781b4647fff32
KomalSwami47 <swamikomal47@gmail.com>
2020-07-24 11:38:30 +0530

    added all the folders

---
+++ js/new.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	
+	ler hamberger = document.querySelector('.hamberger');
+	let cross = document.querySelector('.cross');
+	let mobileNav= document.querySelector('.mobile-nav');
+	
+	
+	hamberger.addEventListener('click',function(){
+		mobileNav.classList.add('open');
+		
+	})
+	
+	cross.addEventListener('click',function(){
+		mobileNav.classList.remove('open');
+		
+	})
+})
